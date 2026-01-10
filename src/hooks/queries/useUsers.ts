45494c8d40d8c75aef50ca511abe9b8061d9415f
Omar Taima <itzdiaz2018@gmail.com>
2026-01-11 01:01:36 +0200

    Update useUsers.ts

--- src/hooks/queries/useUsers.ts
+++ src/hooks/queries/useUsers.ts
@@ -225,8 +225,6 @@ export function useUpdateUserCompanies() {
 
 // Add company access to user
 export function useAddUserCompany() {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: ({
       userId,
@@ -243,8 +241,6 @@ export function useAddUserCompany() {
 
 // Remove company access from user
 export function useRemoveUserCompany() {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: ({
       userId,
