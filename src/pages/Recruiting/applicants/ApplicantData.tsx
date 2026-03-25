40ca4b454001dfd4bd3c65fe0602b1cc99504cb9
Omar Taima <itzdiaz2018@gmail.com>
2026-03-25 16:01:22 +0200

    Update ApplicantData.tsx

--- src/pages/Recruiting/applicants/ApplicantData.tsx
+++ src/pages/Recruiting/applicants/ApplicantData.tsx
@@ -35,7 +35,7 @@ import InterviewScheduleModal from '../../../components/modals/InterviewSchedule
 import CommentModal from '../../../components/modals/commentmodal';
 import InterviewSettingsModal from '../../../components/modals/InterviewSettingsModal';
 import StatusChangeModal from '../../../components/modals/StatusChangeModal';
-import StatusHistory from './StatusHistory';
+import StatusHistory from './StatusHistory.tsx';
 import CustomResponses from './CustomResponses';
 
 // Simple Quill editor integration (dynamic import to avoid react-quill)
